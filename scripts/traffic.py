ee6512c21344e7294db211414017b68a20252bc8
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2021-09-28 16:43:40 +0000

    Creates two functions to save and populate traffic table

--- scripts/traffic.py
+++ scripts/traffic.py
@@ -1,15 +1,7 @@
 import pandas as pd
 from sql_utilities import create_conn, insert_to
 
-def fill_traf():
-    script =  """INSERT INTO `stations` (`station_id`, `weekday_id`, `hours`, `min`, `sec`, `tot_flow`) VALUES (%s, %s, %s, %s)"""
-    myConnection = create_conn( 
-                                host='localhost', 
-                                user='warehouse', 
-                                password='password', 
-                                database='Warehouse'
-                                )
-
+def create_traf_data():
     traffic = pd.read_csv('data/weekday.csv')
     day_week = range(1,7)
 
@@ -20,6 +12,17 @@ def fill_traf():
 
     traffic['weekday'] = days
 
+    traffic.to_csv('data/traffic.csv')
+
+def fill_traf():
+    script =  """INSERT INTO `stations` (`station_id`, `weekday_id`, `hours`, `min`, `sec`, `tot_flow`) VALUES (%s, %s, %s, %s)"""
+    myConnection = create_conn( 
+                                host='localhost', 
+                                user='warehouse', 
+                                password='password', 
+                                database='Warehouse'
+                                )
+    traffic = pd.read_csv('data/traffic.csv')
     for row,k in traffic.iterrows():
         vals = (k['ID'], k['weekday'], k['hours'], k['min'], k['sec'], k['totalflow'])
         insert_to(connection=myConnection, vals=vals, script=script)
