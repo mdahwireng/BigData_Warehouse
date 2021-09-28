3bc6cb1f5aced38f830ebc54882206db01e53ad9
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2021-09-28 16:25:27 +0000

    Creates a function for filling traffic table

---
+++ scripts/traffic.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sql_utilities import create_conn, insert_to
+
+def fill_traf():
+    script =  """INSERT INTO `stations` (`station_id`, `weekday_id`, `hours`, `min`, `sec`, `tot_flow`) VALUES (%s, %s, %s, %s)"""
+    myConnection = create_conn( 
+                                host='localhost', 
+                                user='warehouse', 
+                                password='password', 
+                                database='Warehouse'
+                                )
+
+    traffic = pd.read_csv('data/weekday.csv')
+    day_week = range(1,7)
+
+    days = []
+    for i in range(int(len(traffic)/6)):
+        for k in day_week:
+            days.append(k)
+
+    traffic['weekday'] = days
+
+    for row,k in traffic.iterrows():
+        vals = (k['ID'], k['weekday'], k['hours'], k['min'], k['sec'], k['totalflow'])
+        insert_to(connection=myConnection, vals=vals, script=script)
